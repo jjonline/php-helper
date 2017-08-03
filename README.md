75ce49b810bcb3b29ac4cb27c058ea9dcd8d6d62
Jea杨 <jjonline@jjonline.cn>
2017-08-03 17:34:33 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# phpHelper
+# php-helper
 常用PHP工具函数和类
+
+`摸索composer适配库代码过程中，尚未提交至composer list。。`
---
+++ src/function/FilterValid.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * 常用表单过滤和检测函数
+ * FileName FilterValid.php
+ * @authors Jea杨 (JJonline@JJonline.Cn)
+ * @date    2017-08-03 17:32:38
+ * @version $Id$
+ */
