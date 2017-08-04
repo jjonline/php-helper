68233996065573d91b81e155d4dc6c5a4a55fad3
Jea杨 <jjonline@jjonline.cn>
2017-08-04 13:31:04 +0800

    添加time_ago方法：将时间戳转换为xx时间前表示法

--- src/function/Tools.php
+++ src/function/Tools.php
@@ -41,4 +41,31 @@ class Tools
         $str = str_replace(array('<p>', '</p>', '<br>', '<br/>', '<br />'), '', $str);
         return '<p>'.preg_replace("/([\n|\r\n|\r]{1,})/i", "</p>\n<p>", trim($str)).'</p>';
     }
+
+    /**
+     * 将一个Unix时间戳转换成“xx前”模糊时间表达方式
+     * @param  mixed $timestamp Unix时间戳
+     * @return boolean
+     */
+    function time_ago($timestamp)
+    {
+        $etime = time() - $timestamp;
+        if ($etime < 1) return '刚刚';
+            $interval = array (
+            12 * 30 * 24 * 60 * 60  =>  '年前 ('.date('Y-m-d', $timestamp).')',
+            30 * 24 * 60 * 60       =>  '个月前 ('.date('m-d', $timestamp).')',
+            7 * 24 * 60 * 60        =>  '周前 ('.date('m-d', $timestamp).')',
+            24 * 60 * 60            =>  '天前',
+            60 * 60                 =>  '小时前',
+            60                      =>  '分钟前',
+            1                       =>  '秒前'
+        );
+        foreach ($interval as $secs => $str) {
+            $d = $etime / $secs;
+            if ($d >= 1) {
+                $r = round($d);
+                return $r . $str;
+            }
+        }
+    }
 }
