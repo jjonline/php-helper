a7e908af9ca25965f67446fdfdf23ec59b717f90
Jea杨 <jjonline@jjonline.cn>
2017-08-04 16:54:49 +0800

    Http类初步结构

---
+++ src/library/Http.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Http操纵类：
+ *     1、发送get请求
+ *     2、发送post请求
+ *     3、上传文件
+ *     4、下载文件
+ * @authors Jea杨 (JJonline@JJonline.Cn)
+ * @date    2017-08-04 16:51:07
+ * @version $Id$
+ */
+namespace jjonline\library;
+
+class Http
+{
+
+}
