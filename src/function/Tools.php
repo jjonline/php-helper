c03c07d29013d6dcc28a2887e6e09c183bd1201f
Jea杨 <jjonline@jjonline.cn>
2017-08-04 13:28:48 +0800

    添加nl2p方法：类似nl2br方法

--- src/function/Tools.php
+++ src/function/Tools.php
@@ -30,4 +30,15 @@ class Tools
         }
         return false;
     }
+
+    /**
+     * nl2br的类似函数，将(多个)换行替换成p标签
+     * @param  string $str
+     * @return string
+     */
+    function nl2p($str)
+    {
+        $str = str_replace(array('<p>', '</p>', '<br>', '<br/>', '<br />'), '', $str);
+        return '<p>'.preg_replace("/([\n|\r\n|\r]{1,})/i", "</p>\n<p>", trim($str)).'</p>';
+    }
 }
