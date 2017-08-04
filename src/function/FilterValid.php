1d80ed249af6a27853d8df0ceec38c3e5d3bd53c
Jea杨 <jjonline@jjonline.cn>
2017-08-04 11:06:49 +0800

    添加检查字符串是否为中文汉字的方法is_chinese

--- src/function/FilterValid.php
+++ src/function/FilterValid.php
@@ -26,7 +26,7 @@ class FilterValid {
         */
         #return !!filter_var($mail,FILTER_VALIDATE_EMAIL);
         #正则方式 '/^\w+(?:[-+.]\w+)*@\w+(?:[-.]\w+)*\.\w+(?:[-.]\w+)*$/' 邮箱域名顶级后缀至少两个字符
-        return preg_match('/^\w+(?:[-+.]\w+)*@\w+(?:[-.]\w+)*\.\w{2,}$/',$mail)===1;
+        return preg_match('/^\w+(?:[-+.]\w+)*@\w+(?:[-.]\w+)*\.\w{2,}$/',$mail) === 1;
     }
 
     /**
@@ -38,7 +38,7 @@ class FilterValid {
     {
         #Fixed 171 170x
         #详见：http://digi.163.com/15/0812/16/B0R42LSH00162OUT.html
-        return preg_match('/^13[\d]{9}$|14^[0-9]\d{8}|^15[0-9]\d{8}$|^18[0-9]\d{8}$|^170[015789]\d{7}|^171[89]\d{7}|^17[678]\d{8}$/',$phone)===1;
+        return preg_match('/^13[\d]{9}$|14^[0-9]\d{8}|^15[0-9]\d{8}$|^18[0-9]\d{8}$|^170[015789]\d{7}|^171[89]\d{7}|^17[678]\d{8}$/',$phone) === 1;
     }
 
     /**
@@ -54,7 +54,7 @@ class FilterValid {
      */
     public static function is_url_valid($url)
     {
-        return preg_match('/^http[s]?:\/\/(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3}|(?:[0-9a-z_!~*\'()-]+\.)*(?:[0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.[a-z]{2,6})(?::[0-9]{1,4})?(?:(?:\/\?)|(?:\/[0-9a-zA-Z_!~\*\'\(?:\)\.;\?:@&=\+\$,%#-\/]*)?)$/i',$url)===1;
+        return preg_match('/^http[s]?:\/\/(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3}|(?:[0-9a-z_!~*\'()-]+\.)*(?:[0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\.[a-z]{2,6})(?::[0-9]{1,4})?(?:(?:\/\?)|(?:\/[0-9a-zA-Z_!~\*\'\(?:\)\.;\?:@&=\+\$,%#-\/]*)?)$/i',$url) === 1;
     }
 
     /**
@@ -71,7 +71,7 @@ class FilterValid {
     public static function is_uid_valid($uid, $minLength = 4, $maxLength = 11)
     {
         #正则方式
-        return preg_match('/^[1-9]\d{'.( $minLength - 1 ).','.( $maxLengt - 1 ).'}$/',$uid)===1;
+        return preg_match('/^[1-9]\d{'.( $minLength - 1 ).','.( $maxLengt - 1 ).'}$/',$uid) === 1;
         #函数方式 可能未编译ctype扩展不存在ctype_digit内置函数
         return strlen($uid)>=$minLength && strlen($uid)<=$maxLength && ctype_digit((string)$uid);
     }
@@ -93,6 +93,16 @@ class FilterValid {
         if(strlen($password) > $maxLength || strlen($password) < $minLength) {
             return false;
         }
-        return preg_match('/\d{1,'.$maxLength.'}/',$password)===1 && preg_match('/[a-zA-Z]{1,'.$maxLength.'}/',$password)===1;
+        return preg_match('/\d{1,'.$maxLength.'}/',$password) === 1 && preg_match('/[a-zA-Z]{1,'.$maxLength.'}/',$password) === 1;
+    }
+
+    /**
+     * 检查字符串是否是UTF8编码下的中文
+     * @param  string $string 字符串
+     * @return boolean
+     */
+    public static function is_chinese($str)
+    {
+        return preg_match("/[\x{4e00}-\x{9fa5}]+/u", $str) === 1;
     }
 }
