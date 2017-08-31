4251e7c78b4b4253e7d37e21dc76c317d624c8ce
Jea杨 <jjonline@jjonline.cn>
2017-08-31 16:29:17 +0800

    autoload fixed

--- autoload.php
+++ autoload.php
@@ -12,12 +12,16 @@
 spl_autoload_register(function ($class) {
     // 加载静态方法类
     if (0 === stripos($class, "jjonline\\helper\\")) {
-        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace("jjonline\\helper\\", 'src' . DIRECTORY_SEPARATOR . 'function'.DIRECTORY_SEPARATOR, $class) . '.php';
+        $filename = __DIR__ . DIRECTORY_SEPARATOR
+                  . str_replace("jjonline\\helper\\", 'src' . DIRECTORY_SEPARATOR. 'function' . DIRECTORY_SEPARATOR, $class)
+                  . '.php';
         file_exists($filename) && require($filename);
     }
     // 加载对象类
     if (0 === stripos($class, "jjonline\\library\\")) {
-        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace("jjonline\\library\\", 'src' . DIRECTORY_SEPARATOR . 'library'.DIRECTORY_SEPARATOR, $class) . '.php';
+        $filename = __DIR__ . DIRECTORY_SEPARATOR
+                  . str_replace("jjonline\\library\\", 'src' . DIRECTORY_SEPARATOR . 'library' . DIRECTORY_SEPARATOR, $class)
+                  . '.php';
         file_exists($filename) && require($filename);
     }
 });
