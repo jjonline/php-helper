b080a11f24f4dd9769af0aa09ff75fc3cd3c44fe
Jea杨 <jjonline@jjonline.cn>
2017-08-31 16:14:52 +0800

    不依赖composer的自动加载器实现，引入autoload.php文件即可命名空间方式直接调用

--- autoload.php
+++ autoload.php
@@ -10,5 +10,14 @@
  * 注册自动加载函数
  */
 spl_autoload_register(function ($class) {
-
+    // 加载静态方法类
+    if (0 === stripos($class, "jjonline\\helper\\")) {
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace("jjonline\\helper\\", 'src' . DIRECTORY_SEPARATOR . 'function'.DIRECTORY_SEPARATOR, $class) . '.php';
+        file_exists($filename) && require($filename);
+    }
+    // 加载对象类
+    if (0 === stripos($class, "jjonline\\library\\")) {
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . str_replace("jjonline\\library\\", 'src' . DIRECTORY_SEPARATOR . 'library'.DIRECTORY_SEPARATOR, $class) . '.php';
+        file_exists($filename) && require($filename);
+    }
 });
